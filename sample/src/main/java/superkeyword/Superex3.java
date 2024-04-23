ef0506900f84c7fd34607f5c5110efbfe0c5268f
acer <acer@DEVU>
2024-04-23 08:19:28 +0530

    Superkeyword for Constructor

---
+++ sample/src/main/java/superkeyword/Superex3.java
@@ -0,0 +1,8 @@
+package superkeyword;
+
+public class Superex3 {
+	public Superex3(){
+		System.out.println("This is parent class constructor");
+	}
+
+}
---
+++ sample/src/main/java/superkeyword/Superexm3.java
@@ -0,0 +1,15 @@
+package superkeyword;
+
+public class Superexm3 extends Superex3{
+public Superexm3(){
+	super();
+	System.out.println("This is child class constructor");
+}	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Superexm3 obj=new Superexm3();
+
+	}
+
+}
